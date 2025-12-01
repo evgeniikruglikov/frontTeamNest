1a29bc3fb504d57fe6ca2610807233ff21c7913b
alexandr baranoc <sabbarik5@gmail.com>
2025-12-02 02:33:00 +0300

    проект переделан под vite

--- src/components/Blocks/PersonnelCard.jsx
+++ src/components/Blocks/PersonnelCard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import "../../assets/personnelcard.svg";
-import "PersonnelCard.css";
+// import personnelCardImg from "../../assets/personnelcard.svg";
+import "./PersonnelCard.css";
 
-function PersonnelCard () {
-    
-    return (
-        <div className="personnel-card">
-
-        </div>
-    );
+function PersonnelCard() {
+  return (
+    <div className="personnel-card">
+      {/* <img src={personnelCardImg} alt="Personnel Card" /> */}
+    </div>
+  );
 }
+
+export default PersonnelCard;

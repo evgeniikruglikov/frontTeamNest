f9787b35c0d03256c764c1f356f3e32a45b3417c
evgenii <kruglikowevgenii@gmail.com>
2025-12-02 13:30:38 +0300

    апдейты 2 декабря

--- src/components/Blocks/PersonnelCard.jsx
+++ src/components/Blocks/PersonnelCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import "../../assets/personnelcard.svg";
-import "PersonnelCard.css";
+import cardImage from "../../assets/personnelprofilepic.svg";
+import yellowstar from "../../assets/yellowstar.svg"
+import star from "../../assets/star.svg"
+import "./PersonnelCard.css";
 
 function PersonnelCard () {
     
     return (
         <div className="personnel-card">
-
+            <img className="card-image" src={cardImage} />
+            <div className="card-info">
+                <h3 className="project-name" />
+            </div>
         </div>
     );
 }
+
+export default PersonnelCard;
